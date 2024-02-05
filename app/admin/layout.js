6c28e8d255ce8ad25fe552d2965190969bcc5c0d
AbdulHannan08128 <154576645+AbdulHannan08128@users.noreply.github.com>
2024-02-05 16:09:53 +0530

    V1-f

---
+++ app/admin/layout.js
@@ -0,0 +1,22 @@
+import Sidenav from './Sidenav/Sidenav'
+
+import './admin.css'
+
+export const dynamic = "force-dynamic";
+
+export default function AdminLayout({ children }) {
+    return  (
+    <main>
+      <header>
+      <Sidenav/>
+      </header>
+
+       <div className='content'>
+       <div style={{ overflowY:"scroll"}}>
+      {children}
+      </div>
+      </div>
+      
+    </main>
+    )
+  }
