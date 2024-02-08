081a5f9170dbcb24510d59f55fb1100fc37d2f14
AbdulHannan08128 <154576645+AbdulHannan08128@users.noreply.github.com>
2024-02-08 12:44:46 +0530

    V1-z

--- app/admin/checklist/print/PBTN.jsx
+++ app/admin/checklist/print/PBTN.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function PBTN() {
   return (
-    <button className="print-button hidden md:inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded PHIDE absolute right-5 top-5" onClick={() => window.print()}>
+    <button className="print-button md:inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded PHIDE absolute bottom-5 right-5" onClick={() => window.print()}>
       Print
     </button>
   )
